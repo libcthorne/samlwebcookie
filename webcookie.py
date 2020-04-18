36ba5a1bb615dbc76daf33ccb6463c0c4a5eb39c
Christopher Thorne <libcthorne@gmail.com>
2020-04-18 16:06:38 +0100

    Fix hardcoded URLs

--- webcookie.py
+++ webcookie.py
@@ -80,12 +80,12 @@ if r.status_code == 302:
     r = s.get(
         url=login_url,
         headers={
-            "Host": "fs.auth.wfp.org",
+            "Host": FS_AUTH_HOST,
             "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
             "Accept-Language": "en-GB,en;q=0.5",
             "Accept-Encoding": "gzip, deflate, br",
-            "Referer": "https://vpn.wfp.org/",
+            "Referer": "https://{}".format(VPN_HOST),
             "DNT": "1",
             "Connection": "keep-alive",
             "Upgrade-Insecure-Requests": "1",
