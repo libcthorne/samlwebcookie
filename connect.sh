17007f9f7388ffedf08b944d74a674e9f1f83d35
madebydavid <sutherland.dave@gmail.com>
2020-06-03 15:47:32 +0100

    Bugfix - VPN_PASSWORD must have been set when I built?

--- connect.sh
+++ connect.sh
@@ -3,13 +3,14 @@
 set -e
 
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+envFile="$scriptDir/.env"
 
 # Check for and load the config
-if [ ! -f "$scriptDir/.env" ]; then
+if [ ! -f "$envFile" ]; then
   echo "Please create a .env file as described in the README.md"
   exit 1
 fi
-source "$scriptDir/.env"
+source "$envFile"
 
 # Read user's VPN password
 echo -n "Please enter your VPN password: "
@@ -20,10 +21,11 @@ echo "Running samlwebcookie"
 
 # Get the SAML cookie
 VPN_COOKIE=$(
-  docker run -it --env-file "$scriptDir/.env" samlwebcookie |
-  tee /dev/tty |
-  tail -1
+  docker run -it \
+    --env VPN_PASSWORD=$VPN_PASSWORD \
+    --env-file "$envFile" samlwebcookie | tee /dev/tty | tail -1
 )
 
 # Connect to the VPN
 sudo openconnect -v "$VPN_HOST" --cookie="$VPN_COOKIE"
+
